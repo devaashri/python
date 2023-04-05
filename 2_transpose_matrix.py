c67e710e9a2e1c8e508bb3590f22050345932726
devaashri <129769640+devaashri@users.noreply.github.com>
2023-04-05 23:24:15 +0530

    Add files via upload

---
+++ 2_transpose_matrix.py
@@ -0,0 +1,13 @@
+X = [[4, 6, 7, 8],
+    [3, 7, 2, 7],
+    [7, 3, 7, 5]]
+result = [[0,0,0,0],
+         [0,0,0,0]]
+for i in range(len(X)):
+   for j in range(len(X[0])):
+       for K in range(len(X[0])):
+           for L in range(len(X[0])):
+               result[j][i][K][L] = X[i][j][K][L]
+
+for r in result:
+   print(r)
